ac2f0b3c4ae4932fef65bfc700289ab91e9919cb
Andrei Craiu <craiuandrew97@gmail.com>
2022-12-01 12:47:16 +0200

    add vuetify

--- crs.web-app/src/main.js
+++ crs.web-app/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import vuetify from './plugins/vuetify'
+
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
